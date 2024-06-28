6809a50475e36ac868d2ad31b0bd60438f3ec314
Paul Wentzel <paul.wentzel@gmail.com>
2024-06-28 14:26:54 -0400

    tests passing

--- island.js
+++ island.js
@@ -1,5 +1,5 @@
 function getNeighbors(row, col, matrix) {
-  
+
   // Check top
   // Check top right
   // Check right
@@ -9,16 +9,31 @@ function getNeighbors(row, col, matrix) {
   // Check left
   // Check top left
   // Return neighbors
-  
+
   // Your code here
+  const rv = [];
+
+  const adj = [-1, 0, 1];
+
+  for (const r of adj) {
+    for (const c of adj) {
+      if (!r && !c) continue;
+
+      if (matrix[row + r] && matrix[row + r][col + c]) {
+        rv.push([row + r, col + c]);
+      }
+    }
+  }
+
+  return rv;
 }
 
 function countIslands(matrix) {
-  
+
   // Create a visited set to store visited nodes
   // Initialize count to 0
   // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
+    // If an index contains a 1 and has not been visited,
     // increment island count and start traversing neighbors
       // DO THE THING (increment island count by 1)
       // Initialize a stack with current index
@@ -31,8 +46,35 @@ function countIslands(matrix) {
             // Add neighbor to stack
             // Mark neighbor as visited
   // Return island count
-  
+
   // Your code here
+  const visited = new Set();
+  let count = 0;
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (matrix[r][c] && !visited.has(`${r},${c}`)) {
+        count++;
+        visited.add(`${r},${c}`)
+
+        const stack = [[r,c]];
+
+        while (stack.length > 0) {
+          curr = stack.pop()
+          const neigbors = getNeighbors(curr[0], curr[1], matrix);
+
+          for (const neigbor of neigbors) {
+            if (!visited.has(neigbor.toString())) {
+              stack.push(neigbor);
+              visited.add(neigbor.toString());
+            }
+          }
+        }
+      }
+    }
+  }
+  return count;
+
 }
 
 // Uncomment the lines below for local testing
